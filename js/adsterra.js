3cb7b87376d42a0e24dd1dd499141017a2281309
hexy_mf <thisisafakeemailaddr@gmail.com>
2017-07-26 00:38:45 +0000

    Updated with AdSterra Ads.

---
+++ js/adsterra.js
@@ -0,0 +1,11 @@
+var atOptions = 
+{
+		'key' : '67b8896aa31a27c4342e45f97abeaac2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+};
+
+/* global document location*/
+document.write('<script type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bnserving.com/invoke.js"></script>');
