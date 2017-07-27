45f3054ace8cced8944e318462a9f802426872c7
hexy_mf <thisisafakeemailaddr@gmail.com>
2017-07-27 17:59:11 +0000

    Fixed AdSterra Items

--- js/adsterra.js
+++ js/adsterra.js
@@ -1,11 +1,7 @@
-var atOptions = 
-{
+var	atOptions = {
 		'key' : 'bea857dd6ddf19a66110ab5e0a503ac9',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 };
-
-/* global document location*/
-document.write('<script type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bnserving.com/invoke.js"></script>');
